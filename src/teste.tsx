619ac594e29a0814ff408a7a361d3126c22bc7b4
Vitor Hugo <vitoritouguimaraes@gmail.com>
2024-09-01 15:40:02 -0400

    configurando o ambiente de desenvolvimento

---
+++ src/teste.tsx
@@ -0,0 +1,20 @@
+import styled from 'styled-components'
+
+type BotaoProps = {
+  principal: boolean
+}
+
+const Botao = styled.button<BotaoProps>`
+  background-color: ${(props) => (props.principal ? 'green' : 'blue')};
+`
+
+function Teste() {
+  return (
+    <>
+      <Botao principal>Clique aqui</Botao>
+      <Botao principal={false}>Clique aqui</Botao>
+    </>
+  )
+}
+
+export default Teste
